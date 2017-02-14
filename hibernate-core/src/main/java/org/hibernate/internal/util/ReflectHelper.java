b8ef4835a3632709968025c1dc807ecb9e02087f
Andrea Boriero <andrea@hibernate.org>
2017-02-14 09:52:43 +0000

    HHH-10873 - Remove more o.h.type BasicType implementations

--- hibernate-core/src/main/java/org/hibernate/internal/util/ReflectHelper.java
+++ hibernate-core/src/main/java/org/hibernate/internal/util/ReflectHelper.java
@@ -23,7 +23,7 @@ import org.hibernate.boot.registry.classloading.spi.ClassLoadingException;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.property.access.internal.PropertyAccessStrategyMixedImpl;
 import org.hibernate.property.access.spi.Getter;
-import org.hibernate.type.PrimitiveType;
+import org.hibernate.type.descriptor.java.spi.Primitive;
 import org.hibernate.type.spi.Type;
 
 /**
@@ -322,8 +322,8 @@ public final class ReflectHelper {
 				boolean found = true;
 				for ( int j = 0; j < params.length; j++ ) {
 					final boolean ok = types[j] == null || params[j].isAssignableFrom( types[j].getReturnedClass() ) || (
-							types[j] instanceof PrimitiveType &&
-									params[j] == ( (PrimitiveType) types[j] ).getPrimitiveClass()
+							types[j] instanceof Primitive &&
+									params[j] == ( (Primitive) types[j].getJavaTypeDescriptor() ).getPrimitiveClass()
 					);
 					if ( !ok ) {
 						found = false;
