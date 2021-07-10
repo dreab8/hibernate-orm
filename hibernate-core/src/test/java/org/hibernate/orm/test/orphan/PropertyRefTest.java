e418655fb72261d3f57f9c14fcbaa4c02b8c554e
Andrea Boriero <andrea@hibernate.org>
2021-07-10 20:59:21 +0200

    Re-enabled additional tests

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/orphan/PropertyRefTest.java
@@ -0,0 +1,67 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.orm.test.orphan;
+
+import org.hibernate.testing.TestForIssue;
+import org.hibernate.testing.orm.junit.DomainModel;
+import org.hibernate.testing.orm.junit.NotImplementedYet;
+import org.hibernate.testing.orm.junit.SessionFactoryScope;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author Steve Ebersole
+ */
+@TestForIssue(jiraKey = "HHH-565")
+@DomainModel(
+		xmlMappings = { "org/hibernate/test/orphan/User.hbm.xml", "org/hibernate/test/orphan/Mail.hbm.xml" }
+)
+public class PropertyRefTest {
+
+	@AfterEach
+	public void tearDown(SessionFactoryScope scope) {
+		scope.inTransaction(
+				session -> {
+					session.createQuery( "delete from Mail" ).executeUpdate();
+					session.createQuery( "delete from User" ).executeUpdate();
+				}
+		);
+	}
+
+	@Test
+	@NotImplementedYet
+	public void testDeleteParentWithBidirOrphanDeleteCollectionBasedOnPropertyRef(SessionFactoryScope scope) {
+		User user = new User( "test" );
+		scope.inTransaction(
+				session -> {
+					user.addMail( "test" );
+					user.addMail( "test" );
+					session.save( user );
+				}
+		);
+
+		scope.inTransaction(
+				session -> {
+					User u = session.load( User.class, user.getId() );
+					session.delete( u );
+				}
+		);
+
+		scope.inTransaction(
+				session -> {
+					session.createQuery( "delete from Mail where alias = :alias" )
+							.setParameter( "alias", "test" )
+							.executeUpdate();
+					session.createQuery( "delete from User where userid = :userid" )
+							.setParameter( "userid", "test" )
+							.executeUpdate();
+				}
+		);
+	}
+
+}
