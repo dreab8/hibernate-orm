2268e7fb665fc4ced20decdc91812ade5e846fa4
Andrea Boriero <andrea@hibernate.org>
2024-10-01 09:53:15 +0200

    HHH-18610 SQLGrammarException: Unable to find column position by name: TYPE when using Single Table Inheritance with a strict JDBC driver such as PostgreSQL

--- hibernate-core/src/main/java/org/hibernate/query/results/dynamic/DynamicFetchBuilderContainer.java
+++ hibernate-core/src/main/java/org/hibernate/query/results/dynamic/DynamicFetchBuilderContainer.java
@@ -5,11 +5,19 @@
 package org.hibernate.query.results.dynamic;
 
 import org.hibernate.query.results.FetchBuilder;
+import org.hibernate.sql.results.graph.Fetchable;
 
 /**
  * @author Steve Ebersole
  */
 public interface DynamicFetchBuilderContainer {
+	/**
+	 * Locate an explicit fetch definition for the fetchable
+	 */
+	default FetchBuilder findFetchBuilder(Fetchable fetchable) {
+		return findFetchBuilder( fetchable.getFetchableName() );
+	}
+
 	/**
 	 * Locate an explicit fetch definition for the named fetchable
 	 */
@@ -18,17 +26,42 @@ public interface DynamicFetchBuilderContainer {
 	/**
 	 * Add a property mapped to a single column.
 	 */
-	DynamicFetchBuilderContainer addProperty(String propertyName, String columnAlias);
+	default DynamicFetchBuilderContainer addProperty(Fetchable fetchable, String columnAlias) {
+		return addProperty( fetchable.getFetchableName(), columnAlias );
+	}
+
+	/**
+	 * Add a property mapped to a single column.
+	 */
+	DynamicFetchBuilderContainer addProperty(String fetchableName, String columnAlias);
 
 	/**
 	 * Add a property mapped to multiple columns
 	 */
-	DynamicFetchBuilderContainer addProperty(String propertyName, String... columnAliases);
+	default DynamicFetchBuilderContainer addProperty(Fetchable fetchable, String... columnAliases) {
+		return addProperty( fetchable.getFetchableName(), columnAliases );
+	}
+
+	/**
+	 * Add a property mapped to multiple columns
+	 */
+	DynamicFetchBuilderContainer addProperty(String fetchableName, String... columnAliases);
 
 	/**
 	 * Add a property whose columns can later be defined using {@link DynamicFetchBuilder#addColumnAlias}
 	 */
-	DynamicFetchBuilder addProperty(String propertyName);
+	default DynamicFetchBuilder addProperty(Fetchable fetchable) {
+		return addProperty( fetchable.getFetchableName() );
+	}
 
-	void addFetchBuilder(String propertyName, FetchBuilder fetchBuilder);
+	/**
+	 * Add a property whose columns can later be defined using {@link DynamicFetchBuilder#addColumnAlias}
+	 */
+	DynamicFetchBuilder addProperty(String fetchableName);
+
+	default void addFetchBuilder(Fetchable fetchable, FetchBuilder fetchBuilder) {
+		addFetchBuilder( fetchable.getFetchableName(), fetchBuilder );
+	}
+
+	void addFetchBuilder(String fetchableName, FetchBuilder fetchBuilder);
 }
