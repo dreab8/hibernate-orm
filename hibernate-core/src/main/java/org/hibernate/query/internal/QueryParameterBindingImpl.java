03457f1bcb6faa638464d25d36983a67b21ff1e7
Marco Belladelli <marcobladel@gmail.com>
2024-12-17 09:57:08 +0100

    HHH-17151 Avoid setting explicit temporal bind type for null values

--- hibernate-core/src/main/java/org/hibernate/query/internal/QueryParameterBindingImpl.java
+++ hibernate-core/src/main/java/org/hibernate/query/internal/QueryParameterBindingImpl.java
@@ -322,7 +322,7 @@ public class QueryParameterBindingImpl<T> implements QueryParameterBinding<T>, J
 
 	private void setExplicitTemporalPrecision(TemporalType precision) {
 		explicitTemporalPrecision = precision;
-		if ( bindType == null || JavaTypeHelper.isTemporal( determineJavaType( bindType ) ) ) {
+		if ( bindType != null && JavaTypeHelper.isTemporal( determineJavaType( bindType ) ) ) {
 			bindType = BindingTypeHelper.INSTANCE.resolveTemporalPrecision( precision, bindType, sessionFactory );
 		}
 	}
