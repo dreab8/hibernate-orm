cb9afa9a0464c9f6438c84308d3b63539e4f4b1a
Andrea Boriero <andrea@hibernate.org>
2024-12-17 12:08:30 +0100

    HHH-17151 Add test for issue

--- hibernate-core/src/test/java/org/hibernate/orm/test/jpa/query/DateTimeParameterTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/query/DateTimeParameterTest.java
@@ -4,151 +4,151 @@
  */
 package org.hibernate.orm.test.jpa.query;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.Id;
 import jakarta.persistence.Parameter;
 import jakarta.persistence.Query;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import org.hibernate.testing.orm.junit.EntityManagerFactoryScope;
+import org.hibernate.testing.orm.junit.Jpa;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.hibernate.orm.test.jpa.BaseEntityManagerFunctionalTestCase;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * @author Steve Ebersole
  */
-public class DateTimeParameterTest extends BaseEntityManagerFunctionalTestCase {
+@Jpa(
+		annotatedClasses = {
+				DateTimeParameterTest.Thing.class
+		}
+)
+public class DateTimeParameterTest {
 	private static GregorianCalendar nowCal = new GregorianCalendar();
 	private static Date now = new Date( nowCal.getTime().getTime() );
 
-	@Override
-	protected Class<?>[] getAnnotatedClasses() {
-		return new Class[] { Thing.class };
-	}
-
 	@Test
-	public void testBindingCalendarAsDate() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-
-		try {
-			Query query = em.createQuery( "from Thing t where t.someDate = :aDate" );
-			query.setParameter( "aDate", nowCal, TemporalType.DATE );
-			List list = query.getResultList();
-			assertEquals( 1, list.size() );
-		}
-		finally {
-			em.getTransaction().rollback();
-			em.close();
-		}
-	}
-
-	@Test
-	public void testBindingNulls() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-
-		try {
-			Query query = em.createQuery( "from Thing t where t.someDate = :aDate or t.someTime = :aTime or t.someTimestamp = :aTimestamp" );
-			query.setParameter( "aDate", (Date) null, TemporalType.DATE );
-			query.setParameter( "aTime", (Date) null, TemporalType.DATE );
-			query.setParameter( "aTimestamp", (Date) null, TemporalType.DATE );
-		}
-		finally {
-			em.getTransaction().rollback();
-			em.close();
-		}
-	}
-
-
-	@Test
-	public void testBindingNullNativeQueryPositional() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-
-		try {
-			final Query query = em.createNativeQuery( "update Thing set someDate = ?1 where id = 1" );
-			//noinspection deprecation
-			query.setParameter( 1, (Date) null, TemporalType.DATE );
-			assertEquals( 1, query.executeUpdate() );
-		}
-		finally {
-			em.getTransaction().rollback();
-			em.close();
-		}
-	}
-
-	@Test
-	public void testBindingNullNativeQueryNamed() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-		try {
-			final Query query = em.createNativeQuery( "update Thing set someDate = :me where id = 1" );
-			Parameter<Date> p = new Parameter<>() {
-				@Override
-				public String getName() {
-					return "me";
+	public void testBindingCalendarAsDate(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> {
+					Query query = entityManager.createQuery( "from Thing t where t.someDate = :aDate" );
+					query.setParameter( "aDate", nowCal, TemporalType.DATE );
+					List list = query.getResultList();
+					assertThat( list.size() ).isEqualTo( 1 );
 				}
+		);
+	}
 
-				@Override
-				public Integer getPosition() {
-					return null;
+	@Test
+	public void testBindingNulls(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> {
+					Query query = entityManager.createQuery(
+							"from Thing t where t.someDate = :aDate or t.someTime = :aTime or t.someTimestamp = :aTimestamp" );
+					query.setParameter( "aDate", (Date) null, TemporalType.DATE );
+					query.setParameter( "aTime", (Date) null, TemporalType.DATE );
+					query.setParameter( "aTimestamp", (Date) null, TemporalType.DATE );
+					query.getResultList();
 				}
+		);
 
-				@Override
-				public Class<Date> getParameterType() {
-					return Date.class;
+	}
+
+
+	@Test
+	public void testBindingNullNativeQueryPositional(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> {
+					final Query query = entityManager.createNativeQuery(
+							"update Thing set someDate = ?1 where id = 1" );
+					//noinspection deprecation
+					query.setParameter( 1, (Date) null, TemporalType.DATE );
+					assertThat( query.executeUpdate() ).isEqualTo( 1 );
 				}
-			};
-			//noinspection deprecation
-			query.setParameter( p, null, TemporalType.DATE );
-			assertEquals( 1, query.executeUpdate() );
-		}
-		finally {
-			em.getTransaction().rollback();
-			em.close();
-		}
+		);
+
+		scope.inTransaction(
+				entityManager -> {
+					final Query query = entityManager.createNativeQuery( "select * from Thing where id = 1",
+							Thing.class );
+					List<Thing> list = query.getResultList();
+					assertThat( list.size() ).isEqualTo( 1 );
+					assertThat( list.get( 0 ).someDate ).isNull();
+				}
+		);
 	}
 
-	@Before
-	public void createTestData() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-		em.persist( new Thing( 1, "test", now, now, now ) );
-		em.getTransaction().commit();
-		em.close();
+	@Test
+	public void testBindingNullNativeQueryNamed(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> {
+					final Query query = entityManager.createNativeQuery(
+							"update Thing set someDate = :me where id = 1" );
+					Parameter<Date> p = new Parameter<>() {
+						@Override
+						public String getName() {
+							return "me";
+						}
+
+						@Override
+						public Integer getPosition() {
+							return null;
+						}
+
+						@Override
+						public Class<Date> getParameterType() {
+							return Date.class;
+						}
+					};
+					//noinspection deprecation
+					query.setParameter( p, null, TemporalType.DATE );
+					assertThat( query.executeUpdate() ).isEqualTo( 1 );
+				}
+		);
+
+		scope.inTransaction(
+				entityManager -> {
+					final Query query = entityManager.createNativeQuery( "select * from Thing where id = 1",
+							Thing.class );
+					List<Thing> list = query.getResultList();
+					assertThat( list.size() ).isEqualTo( 1 );
+					assertThat( list.get( 0 ).someDate ).isNull();
+				}
+		);
+
 	}
 
-	@After
-	public void deleteTestData() {
-		EntityManager em = getOrCreateEntityManager();
-		em.getTransaction().begin();
-		em.createQuery( "delete Thing" ).executeUpdate();
-		em.getTransaction().commit();
-		em.close();
+	@BeforeEach
+	public void createTestData(EntityManagerFactoryScope scope) {
+		scope.inTransaction(
+				entityManager -> entityManager.persist( new Thing( 1, "test", now, now, now ) )
+		);
 	}
 
-	@Entity( name="Thing" )
-	@Table( name = "THING" )
+	@AfterEach
+	public void deleteTestData(EntityManagerFactoryScope scope) {
+		scope.getEntityManagerFactory().getSchemaManager().truncate();
+	}
+
+	@Entity(name = "Thing")
+	@Table(name = "THING")
 	public static class Thing {
 		@Id
 		public Integer id;
 		public String someString;
-		@Temporal( TemporalType.DATE )
+		@Temporal(TemporalType.DATE)
 		public Date someDate;
-		@Temporal( TemporalType.TIME )
+		@Temporal(TemporalType.TIME)
 		public Date someTime;
-		@Temporal( TemporalType.TIMESTAMP )
+		@Temporal(TemporalType.TIMESTAMP)
 		public Date someTimestamp;
 
 		public Thing() {
